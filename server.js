e80c7759903717353d8dd0c9575c49969d5be1eb
Rehan Kaleem <rehk@bgs.ac.uk>
2019-05-22 16:10:39 +0100

    Restructued the app & added a place for the dev setup

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ var express = require('express'),
 	app = express(),    
 	http = require('http'),    
 	socketIO = require('socket.io'),    
-	server, io;
+	server, io;	
+var path = require('path');
+var public = path.join(__dirname, 'app');
 
 // Home page (index.html)
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+// Serve static assets and files
+app.use('/app', express.static(public));
+
+// Dev (/app/index.html)
+app.get('/dev', function (req, res) {
+	res.sendFile(__dirname + '/app/index.html');
 });
 
 // Instnatiate server at port 5000
